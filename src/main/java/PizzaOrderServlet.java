aa7f93e52c35b20a79ee34eb4f934a60227d0ac6
Steven Lacy <steven.lacy11@gmail.com>
2021-12-02 15:12:02 -0600

    complete

---
+++ src/main/java/PizzaOrderServlet.java
@@ -0,0 +1,37 @@
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(name = "PizzaOrderServlet", urlPatterns = "/pizza-order")
+public class PizzaOrderServlet extends HttpServlet {
+
+	//Get information from pizza form from jsp
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		request.getRequestDispatcher("/pizzaOrder.jsp").forward(request, response);
+	}
+
+
+//	print out info from form when submitted
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+		response.sendRedirect("/completed.jsp");
+
+		String crust = request.getParameter("crust");
+		String sauce = request.getParameter("sauce");
+		String size = request.getParameter("size");
+		String[] toppings = request.getParameterValues("toppings");
+		String address = request.getParameter("address");
+
+		System.out.println(crust);
+		System.out.println(sauce);
+		System.out.println(size);
+		for (String topping : toppings) {
+			System.out.println(topping);
+		}
+		System.out.println(address);
+
+	}
+}
